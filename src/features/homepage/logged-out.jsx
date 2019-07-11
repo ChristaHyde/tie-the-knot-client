ccbd38549023b21f5d33c3a659da9eab5c24940f
Christa <christahyde@yahoo.com>
2019-07-10 20:36:34 -0600

    added footer

--- src/features/homepage/logged-out.jsx
+++ src/features/homepage/logged-out.jsx
@@ -10,6 +10,8 @@ import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact"
+
 
 
 function MyNavBar() {
@@ -36,6 +38,49 @@ function MyNavBar() {
       </Navbar.Collapse>
     </Navbar>)
 }
+
+const FooterPage = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Footer Content</h5>
+            <p>
+              Here you can use rows and columns here to organize your footer
+              content.
+            </p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Link 1</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+// export default FooterPage;
+
 
 export default function LoggedOut() {
 
@@ -77,6 +122,7 @@ export default function LoggedOut() {
 
       I am not logged in
   </Container>
+    <FooterPage></FooterPage>
   </>)
 
 }
