1a9b825fecadbfaf0f4d38734e2033f6d0231473
Christa <christahyde@yahoo.com>
2019-07-10 18:38:12 -0600

    Added cards, navbar and jumbotron

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 
-import './styles.css'
+//import './styles.css'
 
-export default function Card({ children }) {
-  return <div className="card">
-    {
-      children
-    }
-  </div>
+export default function WeddingCard({ title }) {
+  return (<Card border="primary" style={{ width: '18rem' }}>
+    <Card.Header>{title || "(no title)"}</Card.Header>
+    <Card.Body>
+      <Card.Title>Primary Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  );
 }
--- src/features/homepage/logged-in.jsx
+++ src/features/homepage/logged-in.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Hero from '../../components/hero'
-import Card from '../../components/card'
-import Card2 from '../../components/card2'
-import Card3 from '../../components/card3'
+import WeddingCard from '../../components/card'
 
 export default function LoggedIn() {
   return <div>
     <h1>You are now logged in</h1>
     <Hero>Hello, I am a hero component</Hero>
-    <Card>This is a card</Card>
-    <Card2>This is a card</Card2>
-    <Card3>This is a card</Card3>
+
+    <WeddingCard>This is a card</WeddingCard>
 
   </div>
 
